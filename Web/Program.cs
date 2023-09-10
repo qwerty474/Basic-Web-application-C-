641c236bac0e001d96f9c5853b1c5d2dad7ddbe8
Stupnikova Yelyzaveta <yelyzaveta_stupnikova@epam.com>
2023-09-10 18:00:02 +0300

    added identity

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Web.Areas.Identity.Data;
+using Web.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("WebDBContextConnection") ?? throw new InvalidOperationException("Connection string 'WebDBContextConnection' not found.");
+
+builder.Services.AddDbContext<WebDBContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<WebUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<WebDBContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
